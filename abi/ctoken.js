b2a3eaa0771d77e81a53014d30556151e46c5795
bun919tw <bun919tw@gmail.com>
2021-12-22 16:22:44 +0800

    test/upgrade.js: rename token name and symbol

--- abi/ctoken.js
+++ abi/ctoken.js
@@ -4,4 +4,6 @@ module.exports = [
   'function balanceOf(address account) external view returns (uint)',
   'function _setImplementation(address implementation_, bool allowResign, bytes memory becomeImplementationData) external',
   'function implementation() external view returns (address)',
+  'function name() external view returns (string)',
+  'function symbol() external view returns (string)',
 ];
--- test/upgrade.js
+++ test/upgrade.js
@@ -2,12 +2,14 @@ const { expect } = require("chai");
 const { ethers, waffle } = require("hardhat");
 const ctokenAbi = require('../abi/ctoken');
 const ctokenAdminAbi = require('../abi/ctokenAdmin');
-const erc20Abi = require('../abi/erc20');
+
+function encodeParameters(types, values) {
+  const abi = new ethers.utils.AbiCoder();
+  return abi.encode(types, values);
+}
 
 describe('upgrade', () => {
-  const toWei = ethers.utils.parseEther;
   const provider = waffle.provider;
-  const MAX = ethers.constants.MaxUint256;
 
   let accounts;
   let admin, adminAddress;
@@ -19,15 +21,7 @@ describe('upgrade', () => {
   let cyUsdc;
   let cyUsdt;
 
-  let weth;
-  let dai;
-  let usdc;
-  let usdt;
-
-  let wethHolder;
-  let daiHolder;
-  let usdcHolder;
-  let usdtHolder;
+  let renameDelegate;
 
   const cTokenAdminAddress = '0xA67B44E37200e92e6Da6249d8ae6D48f832A038d';
 
@@ -36,18 +30,6 @@ describe('upgrade', () => {
   const cyDaiAddress = '0x8e595470Ed749b85C6F7669de83EAe304C2ec68F';
   const cyUsdtAddress = '0x48759F220ED983dB51fA7A8C0D2AAb8f3ce4166a';
   const cyUsdcAddress = '0x76Eb2FE28b36B3ee97F3Adae0C69606eeDB2A37c';
-
-  const newImplementationAddress = '0xCA1041f188FfEcC499e8D4D0F08Dd31B0F41c157';
-
-  const wethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-  const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
-  const usdtAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
-  const usdcAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
-
-  const wethHolderAddress = '0xe5F8086DAc91E039b1400febF0aB33ba3487F29A';
-  const daiHolderAddress = '0xe5F8086DAc91E039b1400febF0aB33ba3487F29A';
-  const usdcHolderAddress = '0xbC8d28CD1821be81bc3A54E935CfD3cf686a0194';
-  const usdtHolderAddress = '0x67aB29354a70732CDC97f372Be81d657ce8822cd';
 
   beforeEach(async () => {
     accounts = await ethers.getSigners();
@@ -61,74 +43,47 @@ describe('upgrade', () => {
     cyUsdc = new ethers.Contract(cyUsdcAddress, ctokenAbi, provider);
     cyUsdt = new ethers.Contract(cyUsdtAddress, ctokenAbi, provider);
 
-    weth = new ethers.Contract(wethAddress, erc20Abi, provider);
-    dai = new ethers.Contract(daiAddress, erc20Abi, provider);
-    usdc = new ethers.Contract(usdcAddress, erc20Abi, provider);
-    usdt = new ethers.Contract(usdtAddress, erc20Abi, provider);
-
-    wethHolder = ethers.provider.getSigner(wethHolderAddress);
-    daiHolder = ethers.provider.getSigner(daiHolderAddress);
-    usdcHolder = ethers.provider.getSigner(usdcHolderAddress);
-    usdtHolder = ethers.provider.getSigner(usdtHolderAddress);
+    const delegateeFactory = await ethers.getContractFactory('CRenameDelegate');
+    renameDelegate = await delegateeFactory.deploy();
   });
 
   it('upgrades cyWeth', async () => {
-    await check(cyWeth, cyWethAddress, weth, wethHolderAddress, toWei('0.1'));
+    await check(cyWeth, cyWethAddress, 'Iron Bank Weth', 'ibWeth');
   });
 
   it('upgrades cyDai', async () => {
-    await check(cyDai, cyDaiAddress, dai, daiHolderAddress, toWei('1'));
+    await check(cyDai, cyDaiAddress, 'Iron Bank Dai', 'ibDai');
   });
 
   it('upgrades cyUsdc', async () => {
-    await check(cyUsdc, cyUsdcAddress, usdc, usdcHolderAddress, '1000000');
+    await check(cyUsdc, cyUsdcAddress, 'Iron Bank Usdc', 'ibUsdc');
   });
 
   it('upgrades cyUsdt', async () => {
-    await check(cyUsdt, cyUsdtAddress, usdt, usdtHolderAddress, '1000000');
+    await check(cyUsdt, cyUsdtAddress, 'Iron Bank Usdt', 'ibUsdt');
   });
 
-  async function check(cToken, cTokenAddress, token, tokenHolderAddress, amount) {
+  async function check(cToken, cTokenAddress, name, symbol) {
     const creamMultisig = ethers.provider.getSigner(creamMultisigAddress);
-    const tokenHolder = ethers.provider.getSigner(tokenHolderAddress);
+
+    const oldImplementationAddress = await cToken.implementation();
 
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
       params: [creamMultisigAddress]
     });
 
-    await cTokenAdmin.connect(creamMultisig)._setImplementation(cTokenAddress, newImplementationAddress, true, '0x00');
-    expect(await cToken.implementation()).to.equal(newImplementationAddress);
+    await cTokenAdmin.connect(creamMultisig)._setImplementation(cTokenAddress, renameDelegate.address, true, encodeParameters(['string', 'string'], [name, symbol]));
+    expect(await cToken.implementation()).to.equal(renameDelegate.address);
+
+    await cTokenAdmin.connect(creamMultisig)._setImplementation(cTokenAddress, oldImplementationAddress, true, '0x00');
+    expect(await cToken.implementation()).to.equal(oldImplementationAddress);
+    expect(await cToken.name()).to.equal(name);
+    expect(await cToken.symbol()).to.equal(symbol);
 
     await hre.network.provider.request({
       method: "hardhat_stopImpersonatingAccount",
       params: [creamMultisigAddress]
-    });
-
-    await hre.network.provider.request({
-      method: "hardhat_impersonateAccount",
-      params: [tokenHolderAddress]
-    });
-
-    // Approve.
-    await token.connect(tokenHolder).approve(cTokenAddress, MAX);
-
-    const balance1 = await token.balanceOf(tokenHolderAddress);
-
-    // Mint.
-    await cToken.connect(tokenHolder).mint(amount);
-    const balance2 = await token.balanceOf(tokenHolderAddress);
-    expect(await cToken.balanceOf(tokenHolderAddress)).to.gt(0);
-    expect(balance1.sub(balance2)).to.eq(amount);
-
-    // Redeem.
-    await cToken.connect(tokenHolder).redeemUnderlying(amount);
-    const balance3 = await token.balanceOf(tokenHolderAddress);
-    expect(balance3.sub(balance2)).to.eq(amount);
-
-    await hre.network.provider.request({
-      method: "hardhat_stopImpersonatingAccount",
-      params: [tokenHolderAddress]
     });
   }
 });
